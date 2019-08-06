a6baa0924ad52c18d786aad32047e0aa4bd5f891
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-08-06 18:10:29 +0530

    created db connection api

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const history = require('connect-history-api-fallback');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '/dist')))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/', express.static(path.join(__dirname, 'dist')))
+//app.use(express.static(path.join(__dirname, '/dist')))
 app.use(history())
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+//app.get('/', (req, res) => res.sendFile('index.html'));
 
 app.listen(port, () => console.log(`site running on port ${port}!`));
