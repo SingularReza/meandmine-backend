148b7e44f7177d8664564c3dbf553022964caf5c
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-08-28 23:17:39 +0530

    removing hyphens and dots from file names

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var storage = multer.diskStorage({
         cb(null, './images')
     },
     filename: function(req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        cb(null, Date.now() + '|' + file.originalname.replace(/-|\s/g, ""))
     }
 })
 
@@ -23,11 +23,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/', express.static(path.join(__dirname, 'dist')))
+//app.use('/', express.static(path.join(__dirname, 'dist')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 //app.use(express.static(path.join(__dirname, '/dist')))
 app.use(history())
 
-app.get('/', (req, res) => res.send('backend'))
+//app.get('/', (req, res) => res.send('backend'))
+//app.get('/images/:path', (res, req) => res.send())
 app.get('/article/:id', (req, res) => {
     article.findArticle(req.params.id, function(content) {
         res.send(content)
